08e99359960efbba980f3845665e7eee112514ff
Edu Gargiulo <e.gargiulo@gmail.com>
2019-09-10 10:23:15 -0300

    Finalizado ejercicio

--- ej1.py
+++ ej1.py
@@ -6,4 +6,6 @@ def body(url):
     article = Article(url)
     article.download()
     article.parse()
+    #re.split("[^a-zA-Z\d]",article.text)
+    #re.split("[^a-z\d]",article.text.lower())
     return Counter(re.split(r'[\W\d]+',article.text))
