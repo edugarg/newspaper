8e5d01dd8819c6ec99596e4ab56e4b79341fa8a5
Edu Gargiulo <e.gargiulo@gmail.com>
2019-09-10 09:47:03 -0300

    Realizado el ejercicio

--- ej1.py
+++ ej1.py
@@ -1,5 +1,9 @@
 import re
 from collections import Counter
+from newspaper import Article
 
 def body(url):
-    return Counter(url)
+    article = Article(url)
+    article.download()
+    article.parse()
+    return Counter(re.split(r'[\W\d]+',article.text))
